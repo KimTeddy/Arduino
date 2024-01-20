5d0237f5acc4ea686fa344d54fbab877d6e1a386
TeddyElectronics <kyc20126@gmail.com>
2024-01-21 00:44:27 +0900

    toggle

--- 2024/240120_esp8266_http_light_iot/240120_esp8266_http_light_iot.ino
+++ 2024/240120_esp8266_http_light_iot/240120_esp8266_http_light_iot.ino
@@ -3,7 +3,7 @@
 const char* ssid = "galaxy";
 const char* password = "asdf1234";
 const int ledDesk = 5;
-int brightness = 0;
+int brightness = 0, prev_brightness;
 unsigned int currentBrightness = 0;
 bool isLightOn=false;
 // Create an instance of the server
@@ -43,19 +43,18 @@ void setup() {
 void loop() {
   if(brightness != (int)currentBrightness) {
     //float progress = ((brightness - currentBrightness)>0) ? ((brightness - currentBrightness)/brightness) : (((float)currentBrightness - (float)brightness)/(float)brightness);
-    float plus = ((float)brightness - (float)currentBrightness)/(float)(50);
+    float diff = ((float)brightness - (float)currentBrightness);
+    float plus = diff/(float)(50);
     if(plus < 1 && plus > 0) plus = 1;
     else if(plus > -1 && plus < 0) plus = -1;
+    if(diff>1) plus = 10;
+    if(diff<-1) plus = -10;
     currentBrightness += (int)plus;
-
-
-
-
 
     analogWrite(ledDesk, (int)(currentBrightness));
 
     Serial.print("currentBrightness: "); Serial.print(currentBrightness);
-    Serial.print("\tprogress: "); Serial.print(progress);
+    //Serial.print("\tprogress: "); Serial.print(progress);
     Serial.print("\tplus: "); Serial.print(plus);
     Serial.print("\tbrightness: "); Serial.println(brightness);
     delay(1);
@@ -82,8 +81,18 @@ void loop() {
   else */
   if (req.indexOf("/led.cgi?status=") > 0) {
     brightness = req.substring(req.indexOf('=') + 1).toInt();
+    isLightOn = true;
+    prev_brightness = brightness;
   }
-
+  else if (req.indexOf("/led.cgi?toggle=on") > 0) {
+    isLightOn = true;
+    brightness = prev_brightness;
+  } 
+  else if (req.indexOf("/led.cgi?toggle=off") > 0) {
+    isLightOn = false;
+    prev_brightness = brightness;
+    brightness = 0;
+  }
   // Prepare the response
   //표준 http 응답 헤더 전송 시작
   client.println("HTTP/1.1 200 OK");
@@ -96,33 +105,94 @@ void loop() {
   client.println("<style>");
   client.println("input[type=range] {");
   client.println("  -webkit-appearance: none;");
-  client.println("  appearance: none;");
-  client.println("  width: 10px;");
+  client.println("  width: 80%;");
   client.println("  height: 80%;");
-  client.println("  margin: 0;");
-  client.println("  background-color: yellow;");
+  client.println("  border-radius: 5px;");
+  client.println("  background: #d3d3d3;");
+  client.println("  outline: none;");
+  client.println("  opacity: 0.7;");
+  client.println("  -webkit-transition: 0.2s;");
+  client.println("  transition: opacity 0.2s;");
+  //client.println("  transform: rotate(-90deg);"); // Rotate the range input
+  client.println("}");
+
+  client.println("input[type=range]:hover {");
+  client.println("  opacity: 1;");
   client.println("}");
 
   client.println("input[type=range]::-webkit-slider-thumb {");
   client.println("  -webkit-appearance: none;");
   client.println("  appearance: none;");
-  client.println("  width: 50%;");
-  client.println("  height: 10px;");
-  client.println("  background-color: #4CAF50;");
+  client.println("  width: 20px;");
+  client.println("  height: 20px;");
+  client.println("  border-radius: 50%;");
+  client.println("  background: #2196F3;");
   client.println("  cursor: pointer;");
   client.println("}");
 
-  client.println("input[type=range]:focus {");
-  client.println("  outline: none;");
+  client.println("input[type=range]::-moz-range-thumb {");
+  client.println("  width: 20px;");
+  client.println("  height: 20px;");
+  client.println("  border-radius: 50%;");
+  client.println("  background: #2196F3;");
+  client.println("  cursor: pointer;");
+  client.println("}");
+  
+  client.println(".toggle-switch {");
+  client.println("  position: relative;");
+  client.println("  display: inline-block;");
+  client.println("  width: 60px;");
+  client.println("  height: 34px;");
   client.println("}");
 
+  client.println(".toggle-switch input {");
+  client.println("  opacity: 0;");
+  client.println("  width: 0;");
+  client.println("  height: 0;");
+  client.println("}");
+
+  client.println(".slider {");
+  client.println("  position: absolute;");
+  client.println("  cursor: pointer;");
+  client.println("  top: 0;");
+  client.println("  left: 0;");
+  client.println("  right: 0;");
+  client.println("  bottom: 0;");
+  client.println("  background-color: #ccc;");
+  client.println("  border-radius: 34px;");
+  client.println("  transition: .4s;");
+  client.println("}");
+
+  client.println(".slider:before {");
+  client.println("  position: absolute;");
+  client.println("  content: '';");
+  client.println("  height: 26px;");
+  client.println("  width: 26px;");
+  client.println("  left: 4px;");
+  client.println("  bottom: 4px;");
+  client.println("  background-color: white;");
+  client.println("  border-radius: 50%;");
+  client.println("  transition: .4s;");
+  client.println("}");
+
+  client.println("input:checked + .slider {");
+  client.println("  background-color: #2196F3;");
+  client.println("}");
+
+  client.println("input:focus + .slider {");
+  client.println("  box-shadow: 0 0 1px #2196F3;");
+  client.println("}");
+
+  client.println("input:checked + .slider:before {");
+  client.println("  transform: translateX(26px);");
+  client.println("}");
   client.println("</style>");
   client.println("</head>");
   client.println("<body>");
   client.println("<br />");
   client.println("<form method=\"get\" action=\"/led.cgi\">");
-  client.println("<p> Brightness : <input type=\"range\" name=\"status\" min=\"0\" max=\"255\" value=\"" + String(brightness) + "\" onchange=\"updateBrightness(this.value)\">");
-  client.println("<p> <label class=\"toggle-switch\"><input type=\"checkbox\" name=\"toggle\" value=\"on\" " + String(isLightOn ? "checked" : "") + " onchange=\"toggleLight(this)\"><span class=\"slider\"></span></label> Light On/Off");
+  client.println("<p> <input type=\"range\" name=\"status\" min=\"0\" max=\"255\" value=\"" + String(brightness) + "\" onchange=\"updateBrightness(this.value)\">");
+  client.println("<p> <label class=\"toggle-switch\"><input type=\"checkbox\" name=\"toggle\" value=\"off\" " + String(isLightOn ? "checked" : "") + " onchange=\"toggleLight(this)\"><span class=\"slider\"></span></label> Light On/Off");
   client.println("</form>");
 
   client.println("<script>");
